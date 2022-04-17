7cb70a678f3395f8b42dbe0860c76d1ebc490dd2
SHOHAG1611 <mdshohag16112000@gmail.com>
2022-04-17 16:16:02 +0600

    update home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,6 +7,8 @@ const Home = () => {
     return (
         <div>
             <Banar></Banar>
+            <h1 className='text-center mt-3'>সোহাগ প্রাইভেট সেন্টার</h1>
+            <h3 className='text-center'>Services List</h3>
             <Services></Services>
         </div>
     );
